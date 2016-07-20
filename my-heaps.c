bc8944e1c3a4f9e3d5c41ea583c434a5e279df03
psonawane27 <psonawane@cs.stonybrook.edu>
2016-07-20 12:26:20 -0400

    symmetric heaps

---
+++ my-heaps.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+#define SIZE 1024
+#define NHEAPS 2
+
+
+struct heap_info{
+    void* heap_base;
+    int heap_length;
+    void* heap_curr;
+} *heaps_ptr;
+
+i
+void heap_init(){
+    int i;
+    heaps_ptr = malloc()
+    for ( i=0; i<NHEAPS; i++ ) {
+        malloc(
+    }
+}
+
+void main(){
+    heap_init();    
+}
